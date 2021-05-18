f4e4b14fed4405e5e5815351f26ce5f31f2a265e
FelixHPro <felix.horent.procal@gmail.com>
2021-05-18 09:29:06 +0200

    DessinExemple

---
+++ testAutocad/DessinExemple.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+void addLine();
+void createPolyline();
+void createSolid3d();
+void makeLayer();
+void listObjects();
+void initApp();
+void unloadApp();
+void helloWorld();
+void createFaceSolid();
+void createBoundingBox();
+void createSolid3dProfileIPE();
+void createSolid3dProfileIPN();
+void createSolid3dProfileUPE();
+void createSolid3dProfileRectHollow();
+void createPolyFaceSolid();
+void DeplacementObjet3D(AcDb3dSolid* pSolid);
+void createSolid3dProfileCircHollow();
+void createSolid3dProfileAsyI();
+void CreationSection(AcDb3dSolid* extrusion);
+void CreationVoid(AcDb3dSolid* extrusion);
